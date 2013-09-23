6e8ab81d54bc1522d88c704912b671d57536fcfd
Jim Karg <jkarg@ikarg.com>
2013-09-23 14:56:15 -0500

    Adding ping script

---
+++ Ping.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+	[Parameter(Mandatory=$True)][string]$HostName,
+	[string]$LogFile = "errorLog.html"
+)
+
+Function LogError 
+{
+	Param ([Object]$message)
+	Write-Output $message | ConvertTo-HTML | Add-Content $LogFile
+}
+
+$ping = New-Object System.Net.NetworkInformation.Ping
+
+while($true) {
+	$result = $ping.Send($HostName)	
+	$resultMessage = $result | Select-Object Status, Address, RoundtripTime, @{Name="Date";Expression={$(Get-Date -Format o)}}
+	if ($resultMessage.Status -ne 'success') { LogError($resultMessage) }
+	Write-Output $resultMessage
+	Start-Sleep -Seconds 1
+}
